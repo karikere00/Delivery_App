748c0c2abe781f58dce8114cf5a850d42636281f
karikere00@gmail.com <karikere00@gmail.com>
2023-01-04 12:31:42 -0600

    Initial commit - Initial version of the app with in-progress login activity

---
+++ app/src/main/java/com/deliveryapp/deliveryapp/GetLocationActivity.java
@@ -0,0 +1,14 @@
+package com.deliveryapp.deliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GetLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_location);
+    }
+}
---
+++ app/src/main/java/com/deliveryapp/deliveryapp/MainActivity.java
@@ -0,0 +1,44 @@
+package com.deliveryapp.deliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Assigns the text input layout, text input edit text, and button to variables:
+        TextInputLayout textInputLayout = findViewById(R.id.textInputLayout);
+        TextInputEditText textInputEditText = findViewById(R.id.textInputEditText);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            //Checks user input
+            public void onClick(View view) {
+                String input = textInputEditText.getText().toString();
+                if (input.length() == 6) {
+                    // Proceed to next screen
+                    //Intent object is created
+                    Intent intent = new Intent(MainActivity.this, GetLocationActivity.class);
+                    //Next activity is started
+                    startActivity(intent);
+
+                } else {
+                    textInputLayout.setError("Enter a 6-digit code");
+                }
+            }
+        });
+
+
+    }
+}
