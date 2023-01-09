0d148fee9a112742c93403f2634fa8503407d63d
ans57513 <ansh.chhabra639@topper.wku.edu>
2023-01-09 14:57:33 -0600

    testing

--- app/src/main/java/com/deliveryapp/deliveryapp/GetLocationActivity.java
+++ app/src/main/java/com/deliveryapp/deliveryapp/GetLocationActivity.java
@@ -11,4 +11,6 @@ public class GetLocationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_location);
     }
+
+    // testing
 }
